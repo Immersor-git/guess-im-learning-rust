f6b15d0da683db5df65c7105c08b201d3d1f0f51
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2024-05-19 17:00:04 -0700

    Rust testing

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::btree_map::Range;
+
 fn main() {
     let a = [-1.0; 10];
     let tup = ("Hello","World",a[0]);
@@ -6,4 +8,31 @@ fn main() {
         word2 = tup.1, 
         val = tup.2
     );
+    let a = 20;
+    let a = iterate(a);
+    println!("{a}");
+
+    for x in (0..30) {
+        println!("Fibonacci for {x}: {fib}",fib = fibonacci(x));
+    }
+    let f: f64 = 212.0;
+    println!("Temp: {f}F, {c}C, {f2}F",c=fah_to_cel(f),f2=fah_to_cel(cel_to_fah(f)));
+}
+
+fn fibonacci(n: i32) -> i32 {
+    return 
+    if n == 0 {0} 
+    else if n == 1 {1} 
+    else {fibonacci(n-1) + fibonacci(n-2)};
+}
+
+fn cel_to_fah(cel: f64) -> f64 {
+    return cel/100.0*(212.0-32.0) + 32.0;
+}
+fn fah_to_cel(fah: f64) -> f64 {
+    return (fah - 32.0)/(212.0-32.0)*100.0;
+}
+
+fn iterate(x: i32) -> i32 {
+    x + 1
 }
